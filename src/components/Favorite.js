4602da4f7f34da35dae1817a3633352e17133b6f
bloodyrainday <bloodyrainday@gmail.com>
2024-03-12 11:54:48 +0300

    feat: change src path to the images

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -9,7 +9,7 @@ class Favorite extends React.Component {
       sliders: [
         {
           id: 1,
-          img: 'coffee-slider-1.png',
+          img: require("../images/coffee-slider-1.png"),
           title: 'S’mores Frappuccino',
           desc: 'This new drink takes an espresso and mixes it with brown sugar and cinnamon before being topped with oat milk.',
           price: '$5.50'
@@ -17,7 +17,7 @@ class Favorite extends React.Component {
 
         {
           id: 2,
-          img: 'coffee-slider-2.png',
+          img: require("../images/coffee-slider-2.png"),
           title: 'Caramel Macchiato',
           desc: 'Fragrant and unique classic espresso with rich caramel-peanut syrup, with cream under whipped thick foam.',
           price: '$5.00'
@@ -25,7 +25,7 @@ class Favorite extends React.Component {
 
         {
           id: 3,
-          img: 'coffee-slider-3.png',
+          img: require("../images/coffee-slider-3.png"),
           title: 'Ice coffee',
           desc: 'A popular summer drink that tones and invigorates. Prepared from coffee, milk and ice.',
           price: '$4.50'
