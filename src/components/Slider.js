4d75c22d0705f7ca7963c6d43f0cd760c8033605
bloodyrainday <bloodyrainday@gmail.com>
2024-03-12 11:59:34 +0300

    feat: change the src path of images again

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -5,7 +5,7 @@ class Slider extends React.Component {
   render() {
     return (
       <div className='slider-wrapper'>
-        <img src={this.props.slider.img.default} alt='slider-img' />
+        <img src={this.props.slider.img} alt='slider-img' />
         <div className='favorite-slider-center-line-slide-info'>
           <h4 className='favorite-slider-center-line-slide-info-name'>{this.props.slider.title}</h4>
           <p className='favorite-slider-center-line-slide-info-desc'>{this.props.slider.desc}</p>
