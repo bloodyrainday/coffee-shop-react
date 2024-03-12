54d3fc8448c9c2cef0049e676e930c737a0a9556
bloodyrainday <bloodyrainday@gmail.com>
2024-03-12 13:47:14 +0300

    feat: add some functionality to make slider work

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -34,47 +34,14 @@ class Favorite extends React.Component {
     }
   }
   render() {
-    
+
     return (
         <section className='favorite'>
           <div className='container'>
 
             <h3 className='favorite-title'>Choose your <i>favorite</i> coffee</h3>
 
-            <div className='favorite-slider'>
-
-              <div className='favorite-slider-button left'>
-                <span className='arrow left'></span>
-              </div>
-
-              <div className='favorite-slider-center'>
-                <div className='favorite-slider-center-line'>
-
                 <Sliders sliders={this.state.sliders} />
-                  {/*<div className='favorite-slider-center-line-slide'>
-                    
-                    <Image image={slideImage}/>
-                    <div className='favorite-slider-center-line-slide-desc'>
-                      <h4 className='favorite-slider-center-line-slide-info-name'>S’mores Frappuccino</h4>
-                      <p className='favorite-slider-center-line-slide-info-desc'>This new drink takes an espresso and mixes it with brown sugar and cinnamon before being topped with oat milk.</p>
-                      <p className='favorite-slider-center-line-slide-info-price'>$5.50</p>
-                    </div>
-                  </div>*/}
-
-                </div>
-              </div>
-
-              <div className='favorite-slider-button right'>
-                <span className='arrow right'></span>
-              </div>
-
-            </div>
-
-            <div className='favorite-switch-wrapper'>
-              <span className='favorite-switch'></span>
-              <span className='favorite-switch'></span>
-              <span className='favorite-switch'></span>
-            </div>
 
           </div>
         </section>
--- src/components/Sliders.js
+++ src/components/Sliders.js
@@ -1,17 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Slider from './Slider'
 
-class Sliders extends React.Component {
-  render() {
+
+const Sliders = ({sliders}) => {
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+    const updateIndex = (newIndex) => {
+      if (newIndex < 0) {
+        newIndex = sliders.length - 1;
+      } else if (newIndex >= sliders.length) {
+        newIndex = 0
+      }
+      setActiveIndex(newIndex)
+    }
+
+
     return (
-      <div className='favorite-slider-center-line-slide'>
-        {this.props.sliders.map((slide) => (
-          <Slider slider={slide} key={slide.id}/>
-        )
-        )}
+      <div className='favorite-slider'>
+
+        <div className='favorite-slider-button left'>
+          <span className='arrow left' onClick={() => {updateIndex(activeIndex - 1)}}></span>
+        </div>
+
+
+        <div className='favorite-slider-center'>
+          <div className='favorite-slider-center-line' style={{transform: `translate(-${activeIndex * 100}%)`}}>
+            <div className='favorite-slider-center-line-slide'>
+              {sliders.map((slide) => (
+                <Slider slider={slide} key={slide.id}/>
+              )
+              )}
+            </div>
+          </div>
+
+          <div className='favorite-switch-wrapper'>
+            {sliders.map((item, index) => (
+              <span className={`favorite-switch ${index === activeIndex ? 'active' : ''}`}></span>
+            ))}
+          </div>
+          
+        </div>
+        
+        <div className='favorite-slider-button right'>
+              <span className='arrow right' onClick={() => {updateIndex(activeIndex + 1)}}></span>
+        </div>
+
       </div>
+
     )
-  }
 }
 
 export default Sliders
