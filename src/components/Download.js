696487a8026e8fbb0990bd08d1fc2e73a2c019ef
bloodyrainday <bloodyrainday@gmail.com>
2024-03-13 11:28:01 +0300

    feat: add some style to the download section

--- src/components/Download.js
+++ src/components/Download.js
@@ -8,12 +8,12 @@ export default class Download extends Component {
         <div className='container'>
 
           <div className='download-column1'>
-            <h4 className='download-column1-title'>Download our apps to start ordering</h4>
+            <h4 className='download-column1-title'><i>Download</i> our apps to start ordering</h4>
             <p className='download-column1-desc'>Download the Resource app today and experience the comfort of ordering your favorite coffee from wherever you are</p>
             
             <div className='download-column1-buttons'>
               <span className='download-column1-button-apple'></span>
-              <span className='download-column1-button google'></span>
+              <span className='download-column1-button-google'></span>
             </div>
           </div>
 
