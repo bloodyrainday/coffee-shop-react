ccb0738c0ab13b0c7cfd4ba9c880245f5efe531f
bloodyrainday <bloodyrainday@gmail.com>
2024-03-19 10:44:56 +0300

    fix: change routes again

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
       <div className='wrapper'>
         <Header />
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/coffee-shop-react" element={<Home/>}/>
           <Route path="/menu" element={<Menu/>}/>
         </Routes>
         <Footer />
