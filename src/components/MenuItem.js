2779ec2e141dbb6da0c5a316f15f5c1faf1b88d7
bloodyrainday <bloodyrainday@gmail.com>
2024-03-20 15:33:50 +0300

    feat: include the prices of additives to the total one

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -2,16 +2,27 @@ import React from 'react'
 
 export default function MenuItem({ showCard, setCardClicked}) {
   // let sum = Number(showCard.price).toFixed(2);
-  const [sum, setSum] = React.useState(Number(showCard.price));
-  const calculateSum = () => {
-    if (chosenItem === 1) {
-      setSum(sum + 0.5)
-    } else if (chosenItem === 2)
-    setSum(sum + 0.75);
-  }
+  // const [sum, setSum] = React.useState(Number(showCard.price));
+  // const calculateSum = () => {
+  //   if (chosenItem === 1) {
+  //     setSum(sum + 0.5)
+  //   } else if (chosenItem === 2)
+  //   setSum(sum + 0.75);
+  // }
+  // const [totalSum, setTotalSum] = React.useState(Number(showCard.price).toFixed(2));
+  // const calculateTotalSum = (price) => {
+  //   setTotalSum(totalSum + price);
+  //   console.log(totalSum)
+  // }
   const [chosenItem, setChosenItem] = React.useState(0);
   const choose = (index) => {
     setChosenItem(index)
+  }
+  const [addSugar, setAddSugar] = React.useState(false);
+  const [addCinnamon, setAddCinnamon] = React.useState(false);
+  const [addSyrup, setAddSyrup] = React.useState(false);
+  if (addSugar === true) {
+    (Number(showCard.price) + 0.5).toFixed(2)
   }
   return (
     <div className='menu-item-wrapper' onClick={(e) => {
@@ -43,16 +54,21 @@ export default function MenuItem({ showCard, setCardClicked}) {
           <div className="menu-item-content-column2-add">
             <p className="menu-item-content-column2-add-text">Additives</p>
             <div className="menu-item-content-column2-add-buttons">
-                <button className="menu-item-content-column2-add-buttons-btn"><span>1</span>Sugar</button>
-                <button className="menu-item-content-column2-add-buttons-btn"><span>2</span>Cinnamon</button>
-                <button className="menu-item-content-column2-add-buttons-btn"><span>3</span>Syrup</button>
+                <button className={`menu-item-content-column2-add-buttons-btn ${addSugar ? 'chosen' : ''}`} onClick={() => setAddSugar(!addSugar)}><span>1</span>Sugar</button>
+                <button className={`menu-item-content-column2-add-buttons-btn ${addCinnamon ? 'chosen' : ''}`} onClick={() => setAddCinnamon(!addCinnamon)}><span>2</span>Cinnamon</button>
+                <button className={`menu-item-content-column2-add-buttons-btn ${addSyrup ? 'chosen' : ''}`} onClick={() => setAddSyrup(!addSyrup)}><span>3</span>Syrup</button>
             </div>
           </div>
 
           <div className="menu-item-content-column2-total">
               <p className="menu-item-content-column2-total-text">Total:</p>
-              <p className="menu-item-content-column2-total-price">{`$ ${chosenItem === 1 ? (Number(showCard.price) + 0.5).toFixed(2) :
-                                                                          chosenItem === 2 ? (Number(showCard.price) + 0.75).toFixed(2) : Number(showCard.price).toFixed(2)}`}</p>
+              <p className="menu-item-content-column2-total-price">{`$ ${((chosenItem === 1 ? (Number(showCard.price) + 0.5) :
+                                                                          chosenItem === 2 ? (Number(showCard.price) + 0.75) :
+                                                                          Number(showCard.price)) +
+                                                                          ((addSugar &&  addCinnamon && addSyrup) ? 1.5 :
+                                                                          (addSugar &&  addCinnamon) || (addSyrup &&  addSugar) || (addCinnamon && addSyrup) ? 1 :
+                                                                          addSugar ||  addCinnamon || addSyrup ? 0.5 : 0)).toFixed(2)
+                                                                          }`}</p>
           </div>
 
           <div className="menu-item-content-column2-warning">
