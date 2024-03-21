e9fbcd835bc79f18913f4f14daa7d2e2ae78123a
bloodyrainday <bloodyrainday@gmail.com>
2024-03-21 18:17:29 +0300

    fix: adaptive design for mobile screens

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -9,10 +9,10 @@ function Burger({onClickBurger}) {
       <div className='burger-wrapper'>
         <div className='burger'>
           <ul className='burger-list'>
-            <a className='burger-list-item-link' href='./coffee-shop-react/#favorite' onClick={onClickBurger}><li className='burger-list-item'>Favorite coffee</li></a>
-            <a className='burger-list-item-link' href='./coffee-shop-react/#cozy' onClick={onClickBurger}><li className='burger-list-item'>About</li></a>
-            <a className='burger-list-item-link' href='./coffee-shop-react/#download' onClick={onClickBurger}><li className='burger-list-item'>Mobile app</li></a>
-            <a className='burger-list-item-link' href='./coffee-shop-react/#footer' onClick={onClickBurger}><li className='burger-list-item'>Contact us</li></a>
+            <a className='burger-list-item-link' href='/coffee-shop-react/#favorite' onClick={onClickBurger}><li className='burger-list-item'>Favorite coffee</li></a>
+            <a className='burger-list-item-link' href='/coffee-shop-react/#cozy' onClick={onClickBurger}><li className='burger-list-item'>About</li></a>
+            <a className='burger-list-item-link' href='/coffee-shop-react/#download' onClick={onClickBurger}><li className='burger-list-item'>Mobile app</li></a>
+            <a className='burger-list-item-link' href='/coffee-shop-react/#footer' onClick={onClickBurger}><li className='burger-list-item'>Contact us</li></a>
           </ul>
           <Link className='nav-menu-inner' to="/menu" onClick={onClickBurger}>
                 <p className='nav-menu-inner-text'>Menu</p>
