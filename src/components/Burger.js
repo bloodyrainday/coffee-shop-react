d2cbd3e2dbae6d5b41171721f36e5d835fab7405
bloodyrainday <bloodyrainday@gmail.com>
2024-03-22 16:50:55 +0300

    fix: change anchor links

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -9,10 +9,10 @@ function Burger({onClickBurger}) {
       <div className='burger-wrapper'>
         <div className='burger'>
           <ul className='burger-list'>
-            <Link className='burger-list-item-link' to='./#favorite' onClick={onClickBurger}><li className='burger-list-item'>Favorite coffee</li></Link>
-            <Link className='burger-list-item-link' to='./#cozy' onClick={onClickBurger}><li className='burger-list-item'>About</li></Link>
-            <Link className='burger-list-item-link' to='./#download' onClick={onClickBurger}><li className='burger-list-item'>Mobile app</li></Link>
-            <Link className='burger-list-item-link' to='./#footer' onClick={onClickBurger}><li className='burger-list-item'>Contact us</li></Link>
+            <Link className='burger-list-item-link' to='#favorite' onClick={onClickBurger}><li className='burger-list-item'>Favorite coffee</li></Link>
+            <Link className='burger-list-item-link' to='#cozy' onClick={onClickBurger}><li className='burger-list-item'>About</li></Link>
+            <Link className='burger-list-item-link' to='#download' onClick={onClickBurger}><li className='burger-list-item'>Mobile app</li></Link>
+            <Link className='burger-list-item-link' to='#footer' onClick={onClickBurger}><li className='burger-list-item'>Contact us</li></Link>
           </ul>
           <Link className='nav-menu-inner' to="/menu" onClick={onClickBurger}>
                 <p className='nav-menu-inner-text'>Menu</p>
